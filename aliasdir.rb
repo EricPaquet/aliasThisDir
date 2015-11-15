dce422296d33ccbc7133364da7dd79603d4f0c72
ericpaquet <paquet_eric@hotmail.com>
2015-11-15 12:10:41 -0500

    wip

--- aliasdir.rb
+++ aliasdir.rb
@@ -11,3 +11,20 @@ say_hi("Eric")
 say_hi("Anne")
 
 puts "Current dir: " + Dir.pwd
+
+#open('customProfile', 'a') { |f|
+#  f.puts "Hello, world."
+#}
+
+puts "Dir of this file: " + __dir__
+
+currentPath = ENV["PATH"]
+ENV["PATH"] = currentPath + ":" + __dir__
+puts "Path: " + ENV["PATH"]
+
+puts "PATH="+currentPath + ":" + __dir__
+
+
+# set_var.rb BAR > temp.sh ; . temp.sh
+
+
