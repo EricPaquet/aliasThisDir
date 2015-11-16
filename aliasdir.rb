2b8679ce91c7d1813afe67a33a4c1b437d878c9d
ericpaquet <paquet_eric@hotmail.com>
2015-11-15 22:36:49 -0500

    Get just dir name

--- aliasdir.rb
+++ aliasdir.rb
@@ -11,19 +11,23 @@ say_hi("Eric")
 say_hi("Anne")
 
 puts "Current dir: " + Dir.pwd
+puts "Dir of this file: " + __dir__
+puts "Just dir name: " + File.basename(Dir.getwd)
+
 
 #open('customProfile', 'a') { |f|
 #  f.puts "Hello, world."
 #}
 
-puts "Dir of this file: " + __dir__
-
 currentPath = ENV["PATH"]
+puts "currentPath="+currentPath
+
 ENV["PATH"] = currentPath + ":" + __dir__
 puts "Path: " + ENV["PATH"]
 
 puts "PATH="+currentPath + ":" + __dir__
 
+# exec customProfile
 
 # set_var.rb BAR > temp.sh ; . temp.sh
 
