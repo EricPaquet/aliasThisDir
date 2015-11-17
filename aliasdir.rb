7c3d46075e1ea39986d4ee6eb7ca44957a68d169
Eric Paquet <paquet_eric@hotmail.com>
2015-11-16 22:01:45 -0500

    Commit de l ordi du salon

--- aliasdir.rb
+++ aliasdir.rb
@@ -11,3 +11,18 @@ say_hi("Eric")
 say_hi("Anne")
 
 puts "Current dir: " + Dir.pwd
+
+ENV["toto"]="4"
+
+# echo "hi" > "mycmd.txt"
+# File.write("mycmd.txt", "hi\n")
+File.write("mycmd.txt", "toto=6\n")
+wasGood = system( "echo 'hi'" )
+wasGood = system( "echo 'hi2'" )
+wasGood = system( "cat mycmd.txt" )
+
+# puts "WG " + {wasGood}
+
+wasGood = system( ". mycmd.txt" )
+wasGood = system( "echo apres" )
+
